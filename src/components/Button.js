4516d62df285014542cb791b6c617b930d450b3c
Yunho Cho <ycho0502@gmail.com>
2021-12-13 19:18:36 -0800

    Added first section of About page. LOGO sizing needs to be modified, second section needs to be added

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Button.css";
-import { Link } from "react-router-dom";
-
-function Button() {
-  return <div></div>;
-}
-
-export default Button;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import "./About.css";
+import logo from "../../assets/WAK.png";
 function About() {
   return (
     <div className="about">
-      <h1>About</h1>
+      <section className="first">
+        <div className="about-heading">
+          WE MAKE HOPE
+          <img className="about-logo" src={logo} />
+          <div className="about-content">
+            왁랩은 2019년에 설립된 팬 디자인 클럽으로, 트위치에서 방송중인
+            스트리머 '우왁굳'을 사랑하는 사람들이 모여 만들었습니다.<br></br>
+            디자이너 뿐만이 아니라 일러스트레이터, 성우, 프로그래머 등 각자의
+            분야의 팀원들이 협력하며 의미있는 결과물을 만들기 위해 노력합니다.
+          </div>
+        </div>
+      </section>
+      <section className="second">Second page</section>
     </div>
   );
 }
