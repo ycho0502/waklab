04a1d7e5a769ec1be79fa3a5b38a5e2dcb8f1ff6
Yunho Cho <ycho0502@gmail.com>
2021-12-19 00:56:53 -0800

    img slider added,need more styling

--- src/components/pages/Clothing.js
+++ src/components/pages/Clothing.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { SliderData } from "./SliderData";
+import ImageSlider from "./ImageSlider";
 
 function Clothing() {
   return (
     <div className="clothing">
       <h1>Clothing</h1>
+      <ImageSlider slides={SliderData} />
     </div>
   );
 }
