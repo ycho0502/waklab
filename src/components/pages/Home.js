67011ebf3db10806699d1b7037facd89717c8d15
Yunho Cho <ycho0502@gmail.com>
2021-12-23 01:44:01 -0800

    landing page intro added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Home.css";
+import bgText from "../../assets/back.png";
+import logo from "../../assets/WAK.png";
+import calendarPic from "../../assets/2019_calendar.png";
 
 function Home() {
   return (
@@ -17,6 +20,24 @@ function Home() {
           <h3>WE ARE</h3>
           <h2>WAKLAB</h2>
         </div>
+      </section>
+      <section className="intro">
+        <img className="bgText" src={bgText} />
+        <img className="calendarPic" src={calendarPic} />
+        <img className="waklogo" src={logo}></img>
+        <h1>WE MAKE INNOVATION</h1>
+        <p>
+          왁랩은 2019년에 설립된 팬 디자인 클럽으로, 성장형 퇴물 스트리머
+          '우왁굳'을 사랑하는 사람들이 모여 만들었습니다.
+        </p>
+        <p>
+          디자이너 뿐만이 아니라 일러스트레이터, 성우, 프로그래머 등 각자의
+          분야의 팀원들이 협력하며 의미있는 결과물을 만들기 위해 노력합니다.
+        </p>
+        <p>
+          매년 연말공모전을 통해 달력과 굿즈 제작으로 시청자들에게 새로움을
+          선사하고 있습니다.
+        </p>
       </section>
     </div>
   );
