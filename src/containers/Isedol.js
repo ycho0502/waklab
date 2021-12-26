2c1c8690b84b0670694e6f1922fe5e65a6a69699
JH-th <jh.choi@mxncommerce.com>
2021-12-27 01:58:22 +0900

    Feature : 이세돌 페이지
    - 현재 커밋 이세돌 메인 (미니 프로필) 페이지 엮음 작업 완료

--- src/containers/Isedol.js
+++ src/containers/Isedol.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { isedolProfileData } from "../components/data/isedolProfileData";
+import IsedolProfile from "../components/IsedolProfile";
+import Bg_main from "../assets/isedolMain/Bg_main.png";
+import isedol_edge from "../assets/isedolMain/isedol_edge.png";
+import isedol_title from "../assets/isedolMain/isedol_title.png";
+import arrow from "../assets/isedolMain/gold_arrow.png";
+import "./Isedol.css";
 
 function Isedol() {
+  //내일 작업할거
+  const [chooseIsedol, setChooseIsedol] = useState({});
+  const handleImageClick = (e) => {};
   return (
     <div className="isedol">
-      <h1>Isedol</h1>
+      <img className="edge" src={isedol_edge} alt="edge" />
+      <div className="title">
+        <img className="title_img" src={isedol_title} alt="title" />
+      </div>
+      <div className="isedolMain_bg_container">
+        <img className="isedolMain_bg" src={Bg_main} alt="main" />
+        <IsedolProfile
+          isedolProfileData={isedolProfileData}
+          handleImageClick={handleImageClick}
+        />
+      </div>
+      <div className="arrow">
+        <img src={arrow} alt="arrow" />
+      </div>
+      {/* TODO: 내일(월요일) 작업할 부분<IsedolDetail chooseIsedol={chooseIsedol} /> */}
     </div>
   );
 }
