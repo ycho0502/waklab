bbf7e9f52d33e32658f37026072367a4be6a9254
jeonghun-project <dsjoh111@gmail.com>
2021-12-27 22:50:19 +0900

    Feature : Isedol container init Detail logic

--- src/containers/Isedol.js
+++ src/containers/Isedol.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { isedolProfileData } from "../components/data/isedolProfileData";
 import IsedolProfile from "../components/IsedolProfile";
 import Bg_main from "../assets/isedolMain/Bg_main.png";
@@ -6,28 +6,73 @@ import isedol_edge from "../assets/isedolMain/isedol_edge.png";
 import isedol_title from "../assets/isedolMain/isedol_title.png";
 import arrow from "../assets/isedolMain/gold_arrow.png";
 import "./Isedol.css";
+import ReactPageScroller from "react-page-scroller";
+import { isedolsDetailData } from "../components/data/isedolsDetailData";
+import IsedolDetail from "../components/IsedolDetail";
+import Re_wind_inst from "../assets/RE_WIND_Inst.mp3";
 
 function Isedol() {
-  //내일 작업할거
-  const [chooseIsedol, setChooseIsedol] = useState({});
-  const handleImageClick = (e) => {};
+  const [audio] = useState(new Audio(Re_wind_inst));
+  const [plaing, setPlayer] = useState(true);
+
+  //이세돌 프로필 클릭시 스테이트 변경
+  const [chooseIsedol, setChooseIsedol] = useState({ currentIdol: 0 });
+  const handleIdolClick = (e) => {
+    setChooseIsedol({ currentIdol: e.target.name });
+  };
+
+  const audioPlay = useCallback(async (audio) => {
+    try {
+      await audio.play();
+      audio.volume = 0.3;
+    } catch (Err) {
+      console.error(Err);
+    }
+  }, []);
+
+  const audioPause = useCallback((audio) => {
+    audio.pause();
+  }, []);
+
+  useEffect(() => {
+    plaing ? audioPlay(audio) : audioPause(audio);
+  }, []);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlayer(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlayer(false));
+    };
+  }, []);
+
   return (
     <div className="isedol">
-      <img className="edge" src={isedol_edge} alt="edge" />
-      <div className="title">
-        <img className="title_img" src={isedol_title} alt="title" />
-      </div>
-      <div className="isedolMain_bg_container">
-        <img className="isedolMain_bg" src={Bg_main} alt="main" />
-        <IsedolProfile
-          isedolProfileData={isedolProfileData}
-          handleImageClick={handleImageClick}
-        />
-      </div>
-      <div className="arrow">
-        <img src={arrow} alt="arrow" />
-      </div>
-      {/* TODO: 내일(월요일) 작업할 부분<IsedolDetail chooseIsedol={chooseIsedol} /> */}
+      <ReactPageScroller customPageNumber={chooseIsedol.currentIdol}>
+        <section>
+          <img className="edge" src={isedol_edge} alt="edge" />
+          <div className="title">
+            <img className="title_img" src={isedol_title} alt="title" />
+          </div>
+          <div className="isedolMain_bg_container">
+            <img className="isedolMain_bg" src={Bg_main} alt="main" />
+            <IsedolProfile
+              isedolProfileData={isedolProfileData}
+              handleImageClick={handleIdolClick}
+            />
+          </div>
+          <div className="arrow">
+            <img src={arrow} alt="arrow" />
+          </div>
+        </section>
+        {isedolsDetailData.map((isedol, idx) => (
+          <IsedolDetail
+            key={`${isedol.name}_${idx}`}
+            chooseIsedol={chooseIsedol}
+            isedol={isedol}
+            handleImageClick={handleIdolClick}
+          />
+        ))}
+      </ReactPageScroller>
     </div>
   );
 }
