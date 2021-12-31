e8cec41bee18988716cb672a4ddafa98e1fff3a4
prominence0901 <dambo7913@naver.com>
2021-12-31 15:22:36 +0900

    Team page

--- src/containers/Team.js
+++ src/containers/Team.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Closing from "../components/Closing";
+import Teamcomic from "../components/Teamcomic";
+import { teamcomicData } from "../components/data/teamcomicData";
 
+import "./Team.css";
 function Team() {
   return (
-    <div className="team">
-      <h1>Team</h1>
+    <div className="teambg">
+     <Teamcomic slides={teamcomicData} />
+     <Closing></Closing>
     </div>
-  );
+    
+  );   
 }
 
 export default Team;
