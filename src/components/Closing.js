693f13c8792a1b1b491f15b5ede0f94bc2b519a5
prominence0901 <dambo7913@naver.com>
2021-12-31 15:03:27 +0900

    Team page Closing credits

---
+++ src/components/Closing.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import "./Closing.css";
+
+
+class Closing extends Component {
+
+
+  render() {
+    
+    return (
+<section class="credits">
+  <header>
+    <h1>WAK<em>  </em>LAB</h1>
+  </header>
+
+  <dl class="credits__single">
+    <dt>Directed by</dt>
+    <dd>헬로제이</dd>
+    <dt>WEB Design by</dt>
+    <dd>헬로제이</dd>
+    <dt>Product Manager</dt>
+    <dd>헬로제이</dd>
+  </dl>
+
+  <h2>달력 팀</h2>
+  <dl class="credits__double">
+    <dt>커버 & 타이틀</dt>
+    <dd>안동조</dd>
+    <dt>오즈의 마법사</dt>
+    <dd>왁툴루</dd>
+    <dt>라푼젤</dt>
+    <dd>은빈</dd>
+    <dt>알라딘</dt>
+    <dd>낑깡맛</dd>
+    <dt>피터팬</dt>
+    <dd>벙렝</dd>
+    <dt>플랜더스의 개</dt>
+    <dd>레서</dd>
+    <dt>백설공주</dt>
+    <dd>탄파</dd>
+    <dt>잠자는 숲속의 미녀</dt>
+    <dd>에수리</dd>
+    <dt>어린왕자</dt>
+    <dd>실팔</dd>
+    <dt>피노키오</dt>
+    <dd>흑</dd>
+    <dt>신데렐라</dt>
+    <dd>넷</dd>
+  </dl>
+
+  <h2>굿즈 팀</h2>
+  <dl class="credits__double">
+    <dt>디자인</dt>
+    <dd>헬로제이</dd>
+    <dd>임경섭</dd>
+    <dt>반팔 크롭티 & 에코백 일러스트</dt>
+    <dd>문택</dd>
+    <dt>담요 일러스트</dt>
+    <dd>은빈</dd>
+    <dt>장패드 일러스트</dt>
+    <dd>퐁당이</dd>
+  </dl>
+
+
+  <h2>이세계 아이돌 굿즈 팀</h2>
+  <dl class="credits__double">
+    <dt>디자인</dt>
+    <dd>헬로제이</dd>
+    <dt>포스터 일러스트</dt>
+    <dd>낑깡맛</dd>
+    <dt>아크릴 스탠드 일러스트</dt>
+    <dd>에수리</dd>
+    <dt>스티커 일러스트</dt>
+    <dd>혓바늘</dd>
+
+    <dt>아이돌 굿즈 자문</dt>
+    <dd>밀크라지</dd>
+    <dd>혓바늘</dd>
+    <dt>포토카드 일러스트</dt>
+    <dd>여비날</dd>
+    <dt>포토카드 친필 레터링</dt>
+    <dd>이세계 아이돌</dd>
+  </dl>
+
+  <h2>촬영 팀</h2>
+  <dl class="credits__double">
+    <dt>촬영</dt>
+    <dd>손선비</dd>
+    <dt>모델</dt>
+    <dd>애공</dd>
+    <dd>0gam</dd>
+    <dd>안동조</dd>
+  </dl>
+
+  <h2>웹개발 팀</h2>
+  <dl class="credits__single">
+    <dd>DOPEBOY</dd>
+    <dd>광도리</dd>
+    <dd>한국김치맛있다</dd>
+  </dl>
+
+  <h2>영상 팀</h2>
+  <dl class="credits__double">
+    <dt>영상 제작</dt>
+    <dd>헬로제이</dd>
+    <dt>영상 나레이션</dt>
+    <dd>투미츠</dd>
+  </dl>
+
+  <h2>Special Thanks</h2>
+  <ul class="credits__inline">
+    <li>여비날</li>
+    <li>혓바늘</li>
+    <li>주르르</li>
+    <li>고세구</li>
+    <li>징버거</li>
+    <li>릴파</li>
+    <li>아이네</li>
+    <li>비챤</li>
+  </ul>
+
+
+
+
+  <footer>
+    <h1>Thank you for watching</h1>
+  </footer>
+</section>
+
+      
+    );
+  }
+}
+
+export default Closing;
